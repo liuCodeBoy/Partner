0b2171e397c7994aeb2419ee9d507089d2b770e5
Weslie <iweslie@vip.qq.com>
2018-01-24 21:37:46 +0800

    add many view controllers in profile storyboard

--- Partner/Partner/Profile/Controller/Settings/ModifyPasswordViewController.swift
+++ Partner/Partner/Profile/Controller/Settings/ModifyPasswordViewController.swift
@@ -80,6 +80,20 @@ open class ShadowButton: UIButton {
         }
     }
     
+    @IBInspectable
+    public var borderColor: UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    public var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
+    }
+    
     public func reverseSelected() {
         // add shadow
         self.cornerRadius    = 4
@@ -100,6 +114,67 @@ open class ShadowButton: UIButton {
         // set color
         self.backgroundColor = #colorLiteral(red: 0.5411764979, green: 0.631372571, blue: 0.6980392337, alpha: 1)
         self.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+    }
+}
+
+@IBDesignable
+open class ShadowView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.white
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    @IBInspectable
+    public var cornerRadius: CGFloat = 2.0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    @IBInspectable
+    public var shadowOpacity: Float = 0.5 {
+        didSet {
+            layer.shadowOpacity = self.shadowOpacity
+        }
+    }
+    
+    @IBInspectable
+    public var shadowOffset: CGSize = CGSize(width: 10, height: 10) {
+        didSet {
+            layer.shadowOffset = self.shadowOffset
+        }
+    }
+    
+    @IBInspectable
+    public var shadowColor: UIColor = UIColor.gray {
+        didSet {
+            layer.shadowColor = self.shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    public var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = self.shadowRadius
+        }
+    }
+    
+    @IBInspectable
+    public var borderColor: UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    public var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
     }
 }
 
---
+++ Partner/Partner/Profile/Controller/TempTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TempTableViewCell.swift
+//  Partner
+//
+//  Created by Weslie on 24/01/2018.
+//
+
+import UIKit
+
+class TempTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
